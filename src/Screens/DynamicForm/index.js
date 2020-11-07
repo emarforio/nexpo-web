7578a42b6e79a12b08020a329cc893c1f104105b
Adrian Göransson <ad8223go-s@student.lu.se>
2020-11-07 15:49:01 +0100

    Add response object

--- src/Screens/DynamicForm/index.js
+++ src/Screens/DynamicForm/index.js
@@ -16,6 +16,7 @@ export default function() {
   const [error, setError] = useState(null);
   const [fetching, setFetching] = useState(true);
   const [form, setForm] = useState(null);
+  const [response, setResponse] = useState({});
 
   useEffect(() => {
     async function fetchData() {
