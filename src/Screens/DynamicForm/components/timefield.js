c33a094b07885cd11faf30cfaca296a62b3b10f1
Adrian Göransson <ad8223go-s@student.lu.se>
2020-11-07 15:22:31 +0100

    Timepicker without seconds and 5 minute intervals

--- src/Screens/DynamicForm/components/timefield.js
+++ src/Screens/DynamicForm/components/timefield.js
@@ -2,5 +2,11 @@ import React from 'react';
 import { Form, TimePicker } from 'antd';
 
 export default function({ field }) {
-  return <TimePicker defaultValue={field.placeholder || null} />;
+  return (
+    <TimePicker
+      defaultValue={field.placeholder || null}
+      minuteStep={5}
+      format="HH:mm"
+    />
+  );
 }
